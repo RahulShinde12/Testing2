18823af8f622458234a4c62773ef2e0ce1ec528d
Rahul <shinderahu.9921@gmail.com>
2023-07-15 15:38:47 +0530

    Adding all the files

--- front-end/src/Components/SignUp.js
+++ front-end/src/Components/SignUp.js
@@ -1,25 +1,40 @@
-import React,{useState} from "react"
+import React, { useState } from "react";
 
-const SignUp =()=>{
-    const [name,setName]=useState("");
-    const [mail,setEmail]=useState("");
-    const [phone,setPhone]=useState("");
-    const [password,setPassword]=useState("");
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [mail, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
 
-    const collectData=()=>{
-        console.warn(name,mail,phone,password);
+  const collectData = async () => {
+    console.warn(name, mail, phone, password);
+    try {
+      const response = await fetch("http://localhost:6000/register", {
+        method: 'POST',
+        body: JSON.stringify({ name, mail, phone, password }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const result = await response.json();
+      console.warn(result);
+      //localStorage.setItem("user", JSON.stringify(result));
+      // navigate('/')
+    } catch (error) {
+      console.error('Error:', error);
     }
+  }
 
-    return(
-        <div className="sign-up">
-            <h1>Register</h1>
-            <input className="inputBox" type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Name"/>
-            <input className="inputBox" type="text" value={mail} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email"/>
-            <input className="inputBox" type="text" value={phone} onChange={(e)=>setPhone(e.target.value)} placeholder="Enter Contact Number"/>
-            <input className="inputBox" type="password" value={password} onChange={(e)=>setPassword(e.target.value)}  placeholder="Enter Password"/>
-            <button onClick={collectData} className='submit-btn' type="button">Sign Up</button>
-        </div>
-    )
+  return (
+    <div className="sign-up">
+      <h1>Register</h1>
+      <input className="inputBox" type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Name" />
+      <input className="inputBox" type="text" value={mail} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email" />
+      <input className="inputBox" type="text" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Enter Contact Number" />
+      <input className="inputBox" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" />
+      <button onClick={collectData} className='submit-btn' type="button">Sign Up</button>
+    </div>
+  );
 }
 
 export default SignUp;
